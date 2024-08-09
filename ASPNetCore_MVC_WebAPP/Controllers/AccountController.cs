e4d8e2b4813af797da166b2d9c175a9f41a53bf7
admin@regexmy.com <admin@regexmy.com>
2024-08-09 00:06:34 -0700

    initial app commit

---
+++ ASPNetCore_MVC_WebAPP/Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCore_MVC_WebAPP.Controllers
+{
+    public class AccountController : Controller
+    {
+        public async Task Login(string returnUrl = "/")
+        {
+            await HttpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, new AuthenticationProperties
+            {
+                RedirectUri = returnUrl
+            });
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, new AuthenticationProperties
+            {
+                RedirectUri = Url.Action("Index", "Home")
+            });
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        }
+    }
+}
+
